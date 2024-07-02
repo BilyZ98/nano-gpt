c75cfbcac32c7d2e460a7604d3864fed590c6f2e
bily <zhuangzhutao@gmail.com>
2024-07-02 21:27:48 +0800

    add transfomer implementation

--- gpt.py
+++ gpt.py
@@ -10,10 +10,21 @@ data_file = 'data/input.txt'
 with open(data_file, 'r', encoding='utf-8') as f:
     text = f.read()
 
+# batch_size = 32
+# block_size = 8 # what is the maximum context length for predictions
+# max_iters = 5000
+# eval_interval = 500
+# learning_rate = 1e-3
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# # device = 'cpu'
+# print('torch cuda available', torch.cuda.is_available())
+# eval_iters = 200
+# n_embd = 32
+# head_size = 16
+# dropout = 0.2
 
-
-batch_size = 32
-block_size = 8 # what is the maximum context length for predictions
+batch_size = 64
+block_size = 256 # what is the maximum context length for predictions
 max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
@@ -21,7 +32,11 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # device = 'cpu'
 print('torch cuda available', torch.cuda.is_available())
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_head = 6
+n_layer = 6
+# head_size = 16
+dropout = 0.2
 
 torch.manual_seed(1337)
 
@@ -48,8 +63,6 @@ val_data = data[n:]
 
 
 torch.manual_seed(1337)
-batch_size = 4
-block_size = 8
 
 def get_batch(split):
   data = train_data if split == 'train' else val_data
@@ -100,26 +113,83 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B, T, C = x.shape
         k = self.key(x) #(B, T, C)
         q = self.query(x) #(B, T, C)
-        wei = q @ k.transpose(-2, -1)
+        wei = q @ k.transpose(-2, -1) * C **-0.5
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
         wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = self.dropout(wei)
         v = self.value(x) #(B, T, C)
         out = wei @ v #(B,T,T) @ ( B, T, C) -> (B, T, C)
         return out
 
 
 
+class MultiHeadAttention(nn.Module):
+    """Multiple heads of self-attention in parallel"""
+    def __init__(self,num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+
+    def forward(self, x):
+        out =  torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        out = self.dropout(out)
+        return out
+
+class FeedForward(nn.Module):
+    """a simple linear layer followed by a non-linearity"""
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+                nn.Linear(n_embd, n_embd*4),
+                nn.ReLU(),
+                nn.Linear(4*n_embd, n_embd),
+                nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Block(nn.Module):
+    """Transfomer block: communication followed by computation"""
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffw = FeedForward(n_embd )
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffw(self.ln2(x))
+        return x
 
 class BigramLanguageModel(nn.Module):
   def __init__(self, vocab_size):
     super().__init__()
     self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
     self.position_embedding_table = nn.Embedding(block_size, n_embd)
+    # self.sa = Head(n_embd)
+    # self.sa_heads = MultiHeadAttention(4, n_embd//4)
+    # self.ffw = FeedForward(n_embd)
+    # self.blocks = nn.Sequential(
+    #         Block(n_embd, n_head=4),
+    #         Block(n_embd, n_head=4),
+    #         Block(n_embd, n_head=4),
+    #         nn.LayerNorm(n_embd),
+    # )
+    self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+    self.ln_f = nn.LayerNorm(n_embd)
     self.lm_head = nn.Linear(n_embd, vocab_size)
     # self.feed_forward = nn.Linear()
 
@@ -128,6 +198,11 @@ class BigramLanguageModel(nn.Module):
     tok_emb = self.token_embedding_table(idx) #(B,T,C)
     pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
     x = tok_emb + pos_emb # (B, T, C)
+    # x = self.sa(x)  #(B,T, C)
+    # x = self.sa_heads(x) #(B, T, C)
+    # x = self.ffw(x) #(B, T, C)
+    x = self.blocks(x)
+    x = self.ln_f(x)
     logits = self.lm_head(x) # (B, T, vocab_size)
 
 
@@ -163,10 +238,10 @@ print('model device',first_param.device)
 first_param = next(m.parameters())
 print('m device',first_param.device)
 
-first_param = next(model.parameters())
-logits, loss  = model(xb, yb)
-print(logits.shape)
-print(loss)
+# first_param = next(model.parameters())
+# logits, loss  = model(xb, yb)
+# print(logits.shape)
+# print(loss)
 
 
 
@@ -177,7 +252,6 @@ import time
 start_time = time.time()
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
-batch_size = 32 
 for step in range(max_iters):
     if step % eval_interval == 0 :
         losses = estimate_loss()
